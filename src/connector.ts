3cd692d6d62f1a6e04cff198f46c464bb0975f77
shotaro chiba <s@pws.jp>
2021-01-29 00:37:14 +0900

    feat: Modified interfaces

--- src/connector.ts
+++ src/connector.ts
@@ -6,15 +6,26 @@ interface Context<T> {
     decrypter(content: Buffer, secret: Buffer): Promise<Buffer>,
 }
 
-export abstract class BaseConnector<T> {
+export class BaseConnector<T> {
     protected context: Context<T>
 
     constructor(context: Context<T>) {
         this.context = context
     }
 
-    abstract insert(payload: MnemonicKeyringModel): Promise<Id<MnemonicKeyringModel>>
-    abstract update(id: string, payload: MnemonicKeyringModel): Promise<Id<MnemonicKeyringModel>>
-    abstract findByDid(did: string): Promise<Id<MnemonicKeyringModel> | undefined>
-    abstract deleteById(payload: MnemonicKeyringModel): Promise<MnemonicKeyringModel>
+    public insert(payload: MnemonicKeyringModel): Promise<Id<MnemonicKeyringModel>> {
+        throw new Error()
+    }
+
+    public update(id: string, payload: MnemonicKeyringModel): Promise<Id<MnemonicKeyringModel>> {
+        throw new Error()
+    }
+
+    public findByDid(did: string): Promise<Id<MnemonicKeyringModel> | undefined> {
+        throw new Error()
+    }
+
+    public deleteById(payload: MnemonicKeyringModel): Promise<MnemonicKeyringModel> {
+        throw new Error()
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { BaseConnector } from './connector'
+import { MnemonicKeyringModel, HexKeyPair } from './model'
+import { UNiDInvalidDataError, UNiDInvalidSignatureError, UNiDNotCompatibleError, UNiDNotImplementedError, UNiDNotUniqueError } from './error'
 
 export {
     BaseConnector,
+    MnemonicKeyringModel,
+    HexKeyPair,
+    UNiDInvalidDataError,
+    UNiDInvalidSignatureError,
+    UNiDNotCompatibleError,
+    UNiDNotImplementedError,
+    UNiDNotUniqueError,
 }
