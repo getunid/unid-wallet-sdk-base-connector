a3f7bdd4b6220bdb8b601885d5acfe14332410f8
shotaro chiba <s@pws.jp>
2021-01-29 00:40:55 +0900

    fix: Modified exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { BaseConnector } from './connector'
-import { MnemonicKeyringModel, HexKeyPair } from './model'
+import { MnemonicKeyringModel, Id, HexKeyPair } from './model'
 import { UNiDInvalidDataError, UNiDInvalidSignatureError, UNiDNotCompatibleError, UNiDNotImplementedError, UNiDNotUniqueError } from './error'
 
 export {
     BaseConnector,
     MnemonicKeyringModel,
+    Id,
     HexKeyPair,
     UNiDInvalidDataError,
     UNiDInvalidSignatureError,
